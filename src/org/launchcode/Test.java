b3c2470444e02dc5eca10e5ad2ee94bdbed2dc0e
Subhasish Das <subhasish.besu@gmail.com>
2018-10-05 02:11:13 +0530

    change the function

--- src/org/launchcode/Test.java
+++ src/org/launchcode/Test.java
@@ -4,5 +4,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello Demo");
         System.out.println("Hello Subha");
+        System.out.println("Hello Rai");
+
+        Hello hello = new Hello();
+        hello.displayMsg();
     }
 }
